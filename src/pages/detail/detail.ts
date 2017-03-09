ca97b61f40d9b87570f0f8b3ebf9aac0821d473d
Murph Grainger <murph.grainger@gmail.com>
2017-03-09 14:44:05 -0700

    initial commit

---
+++ src/pages/detail/detail.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-detail',
+  templateUrl: 'detail.html'
+})
+export class DetailPage {
+  item:any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.item = navParams.get('item');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetailPage');
+  }
+
+}
