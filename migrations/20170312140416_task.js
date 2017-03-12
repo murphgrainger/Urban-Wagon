d4b580656d6fd6e36ce85696eb4819c9dad10ccb
Murph Grainger <murph.grainger@gmail.com>
2017-03-12 14:07:21 -0600

    task migration

---
+++ migrations/20170312140416_task.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('task', function(table) {
+    table.increments();
+    table.text('type');
+    table.text('description').notNullable();
+    table.integer('allotted_time').notNullable();
+    table.integer('difficulty').notNullable();
+    table.integer('game_id').references('game.id').unsigned().onDelete('cascade');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('task');
+};
