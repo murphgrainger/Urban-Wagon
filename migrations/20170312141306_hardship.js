4287988978d61dc17b4bc337cdd1b6cc363c0dee
Murph Grainger <murph.grainger@gmail.com>
2017-03-12 14:39:47 -0600

    seeded 4 tasks

--- migrations/20170312141306_hardship.js
+++ migrations/20170312141306_hardship.js
@@ -1,8 +1,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('hardship', function(table) {
     table.increments();
-    table.text('type');
+    table.text('title').notNullable();
     table.text('description').notNullable();
+    table.text('type');
     table.integer('allotted_time').notNullable();
     table.integer('game_id').references('game.id').unsigned().onDelete('cascade');
   });
