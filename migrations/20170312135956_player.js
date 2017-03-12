eb9993dbfd527a8e0f19f773b82b3b750515cecb
Murph Grainger <murph.grainger@gmail.com>
2017-03-12 14:03:12 -0600

    player migration

--- migrations/20170312135956_player.js
+++ migrations/20170312135956_player.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('player', function(table) {
+    table.increments();
+    table.text('trail_name').unique();
+    table.integer('game_id').references('game.id').unsigned().onDelete('cascade');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('player');
 };
