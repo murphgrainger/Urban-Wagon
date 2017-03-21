8387c57e303c7ff9cc21c9334cc86d2516dbfa17
Murph Grainger <murph.grainger@gmail.com>
2017-03-21 11:49:14 -0600

    basic form validation and max player length

--- src/pages/create/create.ts
+++ src/pages/create/create.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+
 import {TabsService} from '../../providers/tabs-service';
-
-
 import { AddplayerPage } from '../addplayer/addplayer';
 
 @Component({
@@ -14,16 +13,17 @@ import { AddplayerPage } from '../addplayer/addplayer';
 export class CreatePage {
   public game:any = {};
 
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public tabs: TabsService) {
+    this.game.difficulty = 1;
+    this.game.players = 1;
+    this.game.goal = 4;
+  }
+
   logForm() {
     this.navCtrl.push(AddplayerPage, {
       game: this.game
     });
   }
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, public tabs: TabsService) {
-    this.game.difficulty = 1;
-    this.game.players = 1;
-  }
-
 
 }
