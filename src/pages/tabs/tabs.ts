bb015e7043d95b965f8aad1dfce38dbd7160e07a
Murph Grainger <murph.grainger@gmail.com>
2017-03-21 16:07:05 -0600

    fixed styling issues

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -13,7 +13,7 @@ import { WinnerPage } from '../winner/winner';
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  tab1Root: any = GuidePage;
+  tab1Root: any = HomePage;
   tab2Root: any = PlayersPage;
   tab3Root: any = GuidePage;
 
